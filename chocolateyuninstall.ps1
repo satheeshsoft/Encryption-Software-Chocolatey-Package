25b6a9621b7313c2a797a38bab38d77468700861
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 03:10:42 +0530

    Update chocolateyuninstall.ps1

--- chocolateyuninstall.ps1
+++ chocolateyuninstall.ps1
@@ -3,5 +3,5 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Encryption Software'
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | % {Write-Warning "- $($_.1000)"}
 }
