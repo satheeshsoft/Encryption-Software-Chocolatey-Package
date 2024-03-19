2ac29c5ee61f93614c64885bc4314a227021aec6
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 03:06:10 +0530

    Update chocolateyinstall.ps1

--- chocolateyinstall.ps1
+++ chocolateyinstall.ps1
@@ -1,14 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'esinstalls.exe'
-$packageName = 'Encryption Software'
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  file         = $fileLocation
-  softwareName  = 'Encryption Software'  
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
 }
 Install-ChocolateyInstallPackage @packageArgs
